74d7678af29d33249fd30dc0f160e205ab351de4
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-04-17 09:54:59 -0500

    fix meetings.  Multiple meetings now work

--- scrape/index.js
+++ scrape/index.js
@@ -36,57 +36,115 @@ function readhtml(dept) {
   return html;
 }
 function parseDept(dept) {
-  //if (dept != "HON") return;  //To just debug one department
+  // if (dept != "BIO") return;  //To just debug one department
   console.log(">>>>DEPT>>>>", dept);
   let html = readhtml(dept);
   let $ = cheerio.load(html);
   let section = {};
+        /*
+          There are two types of list_row classes.
+          The usual type has the following tds
+          * crn                  [0]
+          * course id and url  [1]
+          * section number       [2]
+          * mode                 [3]
+          * title                [4]
+          * credits              [5]
+          * days                 [6]
+          * times                [7]
+          * room                 [8]
+          * instructor           [9]
+          
+          Meeting-only fake list_rows have the following
+          * &nbsp; which spans 6 columns
+          * days
+          * time
+          * room
+          * &nbsp; as dummy cell
+          
+           Fields are all set up as variables and initialized to empty for each section
+           After the row is processed, the crn.length is used
+           to determine if this is real or fake.
+           
+           If it is a real section, then the section is cleared
+           and fields added.  If it is a fake section, then the meeting
+           is added to the meetings array.
+        */
   tds = $("#maincontent table tr").each((i, e) => {
     if (e.attribs.class == "list_row") {
-      //initialize the section
-      section = {};
-      meeting = {};
-      section.dept = dept;
+      //initialize the variables for a list row
+      let realSection = false;
+      let temp_dept = dept;
+      let temp_discipline = ""
+      let temp_crn = ""
+      let temp_course = ""
+      let temp_url = ""
+      let temp_section = ""
+      let temp_mode = ""
+      let temp_title=""
+      let temp_credits = 0
+      let meetings = [] //no need for a temp
+      let meeting = {}  //one meeting
+      let temp_instructor = ""
+
       $(e)
         .children("td")
         .each((ii, ee) => {
           if (ii === 0) {
-            section.crn = $(ee).text().trim();
-
-            console.log("in 0", section.crn, section.crn.length);
-          } else if (ii == 1) {
-            section.course = $(ee).text().trim();
-            section.discipline = section.course.substring(0, 3);
-            section.href = $(ee).children("a").last().attr("href");
-          } else if (ii === 2) {
-            section.sec = $(ee).text().trim();
-          } else if (ii === 3) {
-            section.mode = $(ee).text();
-          } else if (ii === 4) {
-            section.title = $(ee).text();
-          } else if (ii === 5) {
-            section.credits = $(ee).text();
-          } else if (ii === 6) {
+            temp_crn = $(ee).text().trim();
+            realSection = /\d\d\d\d\d+/.test(temp_crn)
+            console.log("Processing:", temp_crn, realSection)
+          } else if (realSection && ii == 1) {
+            temp_course = $(ee).text().trim();
+            temp_discipline = temp_course.substring(0, 3);
+            temp_url = $(ee).children("a").last().attr("href");
+          } else if (realSection && ii === 2) {
+            temp_section = $(ee).text().trim();
+          } else if (realSection && ii === 3) {
+            temp_mode = $(ee).text();
+          } else if (realSection && ii === 4) {
+            temp_title = $(ee).text();
+          } else if (realSection && ii === 5) {
+            section.credits = parseInt($(ee).text().trim());
+          } else if ((realSection && ii === 6) || (!realSection && ii===2)) {
             meeting.day = $(ee).text().trim();
-          } else if (ii === 7) {
+          } else if ((realSection && ii === 7) || (!realSection && ii===3)) {
             meeting.time = $(ee).text().trim();
-          } else if (ii === 8) {
+          } else if ((realSection && ii === 8)||(!realSection && ii === 4)) {
             meeting.room = $(ee).text().trim();
             //console.log(meeting);
             //if this list line is just for an additional meeting time
-            if (section.crn.length >= 5) {
-              section.meetings = [];
-              section.meetings.push(meeting)
-            } else {
-              //Update last element of sections array
-              sections[sections.length - 1].meetings.push(meeting);
-            }
+            // if (realSection) {
+            //   section.meetings = [];
+            //   section.meetings.push(meeting)
+            // } else {
+            //   //Update last element of sections array
+            //   sections[sections.length - 1].meetings.push(meeting);
+            // }
             //if (section) section.meetings.push(meeting);
-          } else if (ii === 9) {
+          } else if (realSection && ii === 9) {
             section.instructor = $(ee).text().trim();
           }
           
         });
+      if (realSection) {
+        section = {};  //initialize section
+        section.dept = temp_dept;
+        section.discipline = temp_discipline
+        section.crn = temp_crn
+        section.id = temp_course
+        section.url=temp_url
+        section.sec = temp_section
+        section.mode = temp_mode
+        section.title = temp_title
+        section.credits = temp_credits
+        section.meetings = meetings
+        section.meetings.push(meeting)
+        section.instructor = temp_instructor = "";
+
+      } else {
+        section.meetings.push(meeting);
+        }
     } else if (e.attribs.class == "detail_row") {
       /*
          There is a big blob of text in the middle of this cell that is
